79b30685f3d01c3da82f44141815d432f8a6974e
Rey Williams <reynaldomwilliams@gmail.com>
2024-03-13 21:43:26 -0700

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -8,8 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "http://localhost:8080"
-RELATIVE_URLS = False
+SITEURL = "https://reywilliams.github.io/"
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
